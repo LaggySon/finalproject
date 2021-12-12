0b94d74cfa736203fb146ab5cc898c8e046fe158
LaggySon <56988314+LaggySon@users.noreply.github.com>
2021-12-12 15:54:29 -0500

    added model

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-###ABOUT
+## ABOUT
+
+predict delay from the line name, origin, and destination of the trip
+predict 'delay_minutes' from 'line', 'from' and 'to'
+
+# Step 1:
+
+Clean data - remove all data that doesn't have delay_minutes
+
+- anything with type=Amtrak
+- remove all unecessary columns
+
+# Step 2:
+
+Our y will be 'delay_minutes' and our X will be 'line', 'from', and 'to'
+
+# Step 3:
+
+We chose SGDRegressor as our model (using https://scikit-learn.org/stable/tutorial/machine_learning_map/index.html)
+
+# Step 4:
+
+Use pandas to get data from csv and input to model
+
+- split data into train and test subsets (80% train, 20% test)
+
+# Step 5:
+
+Predict and plot
+
+# Step 6:
+
+deploy
---
+++ train.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+df = pd.read_csv('./data/2018_03.csv')
+df = df.loc[df['type'] == 'NJ Transit']
+newdata = df.dropna()
+X = newdata[['from_id', 'to_id']]
+y = newdata['delay_minutes']
+
+# from sklearn.feature_extraction import DictVectorizer
+# vec = DictVectorizer(sparse=False, dtype=int)
+# print(vec.fit_transform(df))
+
+
+
+from sklearn.model_selection import train_test_split
+Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size=0.2, random_state=0)
+
+from sklearn.linear_model import SGDRegressor
+model = SGDRegressor()
+model.fit(Xtrain,ytrain)
+y_model = model.predict(Xtest)
+
+from joblib import dump, load 
+dump(model, 'mymodel.joblib') #save  
+model2 = load('mymodel.joblib')  #load
+
+model.predict(np.array([b0.x,b0.y]).reshape(1,-1))
